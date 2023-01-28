d420597ba89873eb2b4d7b59f536caa179d4e773
sumith prabhu <sumithprabhu07@gmail.com>
2023-01-28 20:25:42 +0530

    Code Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import {DeleteStream, CreateStream} from './Components/index';
+import logo from "./logo.svg";
+import "./App.css";
+import { DeleteStream, CreateStream, UpdateStream } from "./Components/index";
 function App() {
   return (
     <div className="App">
+      <header class="header">
+        <h1 class="logo">
+          <a href="#">Flexbox</a>
+        </h1>
+        <ul class="main-nav">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <button className="button">Connect Wallet</button>
+      </header>
       <CreateStream />
+      <UpdateStream />
       <DeleteStream />
     </div>
   );
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,3 @@
 export {default as DeleteStream} from './DeleteStream/DeleteStream';
 export {default as CreateStream} from './CreateStream/CreateStream';
+export {default as UpdateStream} from './UpdateStream/UpdateStream';
